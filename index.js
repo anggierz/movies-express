8e9755782ba03cf778537aa073d6aedb1d9e72c5
anggierz <anggie.rizo.zambrano@gmail.com>
2025-06-03 22:05:03 +0200

    (optional) implement morgan logs

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from "express";
 import moviesRoutes from './routes/moviesRoutes.js';
-
+import morgan from "morgan";
+import { createStream } from "rotating-file-stream";
 
 const PORT = process.env.PORT || 3000;
+const logs_path = process.env.LOGS_PATH;
 
 
 const app = express();
-app.use(express.json());
 
+
+var accessLogStream = createStream('access.log', {
+  interval: '1d', // rotate daily
+  path: logs_path
+});
+app.use(morgan('short', { stream: accessLogStream }));
+
+app.use(express.json());
 
 app.use('/movies', moviesRoutes);
 
